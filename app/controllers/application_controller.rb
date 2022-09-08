ab2647ac9376ef709b08c2f402787fccb2d97ae5
Richard Gichuki <chadgichuki@gmail.com>
2022-09-08 16:43:23 +0300

    Half completed method for top buyer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,12 +11,6 @@ class ApplicationController < Sinatra::Base
     plant.to_json(include: {purchases: {include: :buyer}})
   end
 
-  #Not completed
-  get "plants/most_popular" do
-    plant = Plant.by_purchases.first
-    plant.to_json(include: {purchases: {include: :buyer}})
-  end
-
   post "/plants" do
     new_plant = Plant.create(name: params[:name], species: params[:species], best_climate: params[:best_climate], water_frequency: params[:water_frequency], no_in_stock: params[:no_in_stock], image: params[:image])
     new_plant.to_json
@@ -39,6 +33,11 @@ class ApplicationController < Sinatra::Base
     buyers.to_json
   end
 
+  post "/buyers" do
+    new_buyer = Buyer.create(name: params[:name], contact: params[:contact], feedback: params[:feedback])
+    new_buyer.to_json
+  end
+
   get "/buyers/:id" do
     buyer = Buyer.find(params[:id])
     buyer.to_json(include: {purchases: {include: :plant}})
@@ -49,5 +48,14 @@ class ApplicationController < Sinatra::Base
     buyer.update(feedback: params[:feedback])
     buyer.to_json(include: {purchases: {include: :plant}})
   end
+
+    #Not completed
+    get "plants/most_popular" do
+      plant = Plant.by_purchases.first
+      plant.to_json(include: {purchases: {include: :buyer}})
+    end
+
+    get "plants/top_buyer" do
+    end
 
 end
