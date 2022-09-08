38be25b74b12e6579058ed55b251f4135c4b8af2
Richard Gichuki <chadgichuki@gmail.com>
2022-09-08 16:06:49 +0300

    Handles Get requests for all plants and single plant

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,31 +2,43 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   get "/plants" do
-    { message: "Plants" }.to_json
+    plants = Plant.all
+    plants.to_json
   end
 
   get "/plants/:id" do
-    { message: "Plant moja" }.to_json
+    plant = Plant.find(params[:id])
+    plant.to_json(include: {purchases: {include: :buyer}})
+  end
+
+  get "plants/most_popular" do
+    plant = Plant.by_purchases.first
+    plant.to_json(include: {purchases: {include: :buyer}})
   end
 
   post "/plants" do
-    #{ message: "Plants" }.to_json
+    new_plant = Plant.create(name: params[:name], species: params[:species], )
+    new_plant.to_json
   end
 
   put "/plants/:id" do
     #{ message: "Plants" }.to_json
   end
 
-  delete "/plants" do
-    #{ message: "Plants" }.to_json
+  delete "/plants/:id" do
+    plant = Plant.find(params[:id])
+    plant.destroy
+    plant.to_json
   end
 
   get "/buyers" do
-    { message: "All buyers" }.to_json
+    buyers = Buyer.all
+    buyers.to_json
   end
 
   get "/buyers/:id" do
-    { message: "Buyer Mmoja" }.to_json
+    buyers = Buyer.find(params[:id])
+    buyers.to_json(include: {purchases: {include: :plant}})
   end
 
 end
--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,4 +1,9 @@
 class Plant < ActiveRecord::Base
     has_many :purchases
     has_many :buyers, through: :purchases
+
+    #Order plants by no. of purchases
+    def self.by_purchases
+        self.all.order(purchases: :desc)
+    end
 end
