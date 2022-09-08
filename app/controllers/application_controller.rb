f252524c507afdd7a057ff22e31831489611f753
Richard Gichuki <chadgichuki@gmail.com>
2022-09-08 16:13:22 +0300

    Handles get requests for all buyers and single buyer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,18 +11,21 @@ class ApplicationController < Sinatra::Base
     plant.to_json(include: {purchases: {include: :buyer}})
   end
 
+  #Not completed
   get "plants/most_popular" do
     plant = Plant.by_purchases.first
     plant.to_json(include: {purchases: {include: :buyer}})
   end
 
   post "/plants" do
-    new_plant = Plant.create(name: params[:name], species: params[:species], )
+    new_plant = Plant.create(name: params[:name], species: params[:species], best_climate: params[:best_climate], water_frequency: params[:water_frequency], no_in_stock: params[:no_in_stock], image: params[:image])
     new_plant.to_json
   end
 
-  put "/plants/:id" do
-    #{ message: "Plants" }.to_json
+  patch "/plants/:id" do
+    plant = Plant.find(params[:id])
+    plant.update(no_in_stock: params[:no_in_stock])
+    plant.to_json(include: {purchases: {include: :buyer}})
   end
 
   delete "/plants/:id" do
@@ -37,8 +40,14 @@ class ApplicationController < Sinatra::Base
   end
 
   get "/buyers/:id" do
-    buyers = Buyer.find(params[:id])
-    buyers.to_json(include: {purchases: {include: :plant}})
+    buyer = Buyer.find(params[:id])
+    buyer.to_json(include: {purchases: {include: :plant}})
+  end
+
+  patch "/buyers/:id" do
+    buyer = Buyer.find(params[:id])
+
+    buyer.to_json(include: {purchases: {include: :plant}})
   end
 
 end
