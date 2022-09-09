6db6b41e1ea3776456c7846803dd65f7a12403bf
Richard Gichuki <chadgichuki@gmail.com>
2022-09-09 07:57:51 +0300

    Return plants in alphabetical order

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   get "/plants" do
-    plants = Plant.all
+    plants = Plant.all.order(:name)
     plants.to_json
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,11 +2,11 @@ puts "🌱 Seeding plants, buyers and purchases..."
 
 # Seeding 5 indoor plants
 
-Plant.create(name: "Monstera", species: "Monstera adansonii", best_climate: "Cool and wet climate", water_frequency: "Once a week", no_in_stock: 5, image: "https://commons.wikimedia.org/wiki/File:MonsteraAdansonii.jpg#/media/File:MonsteraAdansonii.jpg")
-Plant.create(name: "Money Plant", species: "Epipremnum aureum", best_climate: "Cool and wet climate", water_frequency: "Once a week", no_in_stock: 10, image: "https://commons.wikimedia.org/wiki/File:Epipremnum_aureum_31082012.jpg#/media/File:Epipremnum_aureum_31082012.jpg")
-Plant.create(name: "Ghost Plant", species: "Monotropa uniflora", best_climate: "Cool and dry", water_frequency: "Once in 2 weeks", no_in_stock: 2, image: "https://commons.wikimedia.org/wiki/File:Indian_pipe_PDB.JPG#/media/File:Indian_pipe_PDB.JPG")
-Plant.create(name: "Peace Lily", species: "Spathiphyllum", best_climate: "Cool and humid", water_frequency: "Once a week", no_in_stock: 20, image: "https://commons.wikimedia.org/wiki/File:Spathiphyllum_cochlearispathum_RTBG.jpg#/media/File:Spathiphyllum_cochlearispathum_RTBG.jpg")
-Plant.create(name: "Cactus", species: "Astrophytum asterias", best_climate: "hot and dry", water_frequency: "Once a month", no_in_stock: 7, image: "https://commons.wikimedia.org/wiki/File:Astrophytum_asterias1.jpg#/media/File:Astrophytum_asterias1.jpg")
+Plant.create(name: "Monstera", species: "Monstera adansonii", best_climate: "Cool and wet climate", water_frequency: "Once a week", no_in_stock: 5, image: "https://cdn.standardmedia.co.ke/images/monday/mrgzearkbazgdw4w621cc47da68d7.jpg")
+Plant.create(name: "Money Plant", species: "Epipremnum aureum", best_climate: "Cool and wet climate", water_frequency: "Once a week", no_in_stock: 10, image: "https://www.herzindagi.com/society-culture/how-to-grow-money-plant-at-home-article-190098")
+Plant.create(name: "Ghost Plant", species: "Monotropa uniflora", best_climate: "Cool and dry", water_frequency: "Once in 2 weeks", no_in_stock: 2, image: "https://www.thespruce.com/thmb/431eRmkyTp30v50o9Az_lxLMbHo=/1578x1052/filters:fill(auto,1)/ghost-plant-succulent-care-and-growing-guide-4684220-closeup-d95cba766e024dbb80be3dbb1e6b70b7.jpg")
+Plant.create(name: "Peace Lily", species: "Spathiphyllum", best_climate: "Cool and humid", water_frequency: "Once a week", no_in_stock: 20, image: "https://hips.hearstapps.com/hmg-prod/images/peace-lilies-royalty-free-image-1644536388.jpg")
+Plant.create(name: "Cactus", species: "Astrophytum asterias", best_climate: "hot and dry", water_frequency: "Once a month", no_in_stock: 7, image: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/cactus-plant-pot-stand-1599155636.jpg?crop=1.00xw:0.753xh;0,0.108xh&resize=1200:*")
 
 # Add the plant ids to an array
 $plant_ids = Plant.all.map { |plant| plant[:id] } 
